7fcb562f7579ac0c8b12cdec90f48af9f9332417
Luke Morales <lukemorales@live.com>
2021-09-16 19:08:25 -0300

    chore: clean up project

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,14 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  "features": {
+    previewCsfV3: true,
+  }
 }
